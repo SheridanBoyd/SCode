a109aeb29f3c346b028d4441593c82cfe50e9ee5
Sheridan Boyd <sheridan@boyd.org.nz>
2017-07-17 13:12:02 +1200

    Added my program to GitHub

---
+++ environmock/lexerparser.py
@@ -0,0 +1,406 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 12:05:32 2017
+
+@author: zavidan
+"""
+
+# Defining the exceptions ##############################################################################################
+class LexingError(Exception):
+    pass
+
+class ParsingError(Exception):
+    pass
+
+# Defining the helper functions for the lexer ##########################################################################
+# This function accepts normal character and returns how many there are
+def numChars(i,textlist):
+    numchars = -1
+    valid = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','?','\'','!','’','.',',','#']
+    while textlist[i] in valid:
+        numchars += 1
+        i += 1
+    return numchars
+
+# This function accepts '\n' , which is a new line, for the lexer
+def exit(i,textlist):
+    numreturns = -1
+    loop = 1
+    while loop == 1:
+        try:
+            if textlist[i] == '\n':
+                numreturns += 1
+                i += 1
+            else:
+                loop = 0
+        except:
+            loop = 0
+    return numreturns
+
+# This function accepts '\r' , which is also a new line, for the lexer
+def rexit(i,textlist):
+    numreturns = -1
+    loop = 1
+    while loop == 1:
+        try:
+            if textlist[i] == '\r':
+                numreturns += 1
+                i += 1
+            else:
+                loop = 0
+        except:
+            loop = 0
+    return numreturns
+
+# This function accepts '\r\n' , which is also a new line, for the lexer
+def rnexit(i,textlist):
+    numreturns = -1
+    loop = 1
+    while loop == 1:
+        try:
+            if textlist[i] == '\r' and textlist[i+1] == '\n':
+                numreturns += 2
+                i += 2
+            else:
+                loop = 0
+        except:
+            loop = 0
+    return numreturns
+
+# This function counts the number of #'s for the lexer
+def oor(i,textlist): #oor means Object or Room
+    numhash = 0
+    while textlist[i] == '#':
+        numhash += 1
+        i += 1
+    return numhash
+
+# Defining the lexer ###################################################################################################
+def lex(text):
+    textlist = list(text)
+    tokenlist = []
+    i = 0
+    while i <= (len(text) -1):
+        if textlist[i] == '#':
+            if textlist[i+1] == '#':
+                tokenlist.append(('NEW_OBJECT','##'))
+                i += 1
+            else:
+                tokenlist.append(('NEW_ROOM','#'))
+        elif textlist[i] == '[':
+            if textlist[i+1] == '[':
+                tokenlist.append(('OPEN_LINK','[['))
+                i += 1
+            else:
+                raise LexingError(textlist[i],i)
+        elif textlist[i] == ']':
+            if textlist[i+1] == ']':
+                tokenlist.append(('CLOSE_LINK',']]'))
+                i += 1
+            else:
+                raise LexingError(textlist[i],i)
+        elif textlist[i] == '{':
+            tokenlist.append(('OPEN_OBJECT','{'))
+        elif textlist[i] == '}':
+            tokenlist.append(('CLOSE_OBJECT','}'))
+
+        elif textlist[i] == '\r':
+            if textlist[i+1] == '\n':
+                numreturns = rnexit(i, textlist)
+                if numreturns == 1:
+                    tokenlist.append(('END_LINE', '\r\n'))
+                else:
+                    tokenlist.append(('EXIT', '\r\n' * ((numreturns + 1)/2)))
+                i += numreturns
+            else:
+                numreturns = rexit(i, textlist)
+                if numreturns == 0:
+                    tokenlist.append(('END_LINE', '\r'))
+                else:
+                    tokenlist.append(('EXIT', '\r' * (numreturns + 1)))
+                    i += numreturns
+        elif textlist[i] == '\n':
+            numreturns = exit(i,textlist)
+            if numreturns == 0:
+                tokenlist.append(('END_LINE','\n'))
+            else:
+                tokenlist.append(('EXIT','\n'*(numreturns+1)))
+                i += numreturns
+        elif textlist[i] == '|':
+            tokenlist.append(('ALT_TEXT','|'))
+        elif textlist[i] == '@':
+            tokenlist.append(('NEW_GUARD','@'))
+        elif textlist[i] == '/':
+            tokenlist.append(('GUARD_LIMIT','/'))
+        else:
+            tempi = i
+            thing = numChars(i,textlist)
+            if thing > -1:
+                i += thing
+                tokenlist.append(('CHARS',''.join(textlist[tempi:i+1])))
+            else:
+                raise LexingError(textlist[i],i)
+        
+        i += 1
+    return tokenlist
+
+
+# Defining the objects for parsing #####################################################################################
+class Endline(object):
+    def __init__(self):
+        pass
+    
+    def __str__(self):
+        return 'END_LINE'
+    
+    def __repr__(self):
+        return '\n'
+    
+    def htmlstr(self,token):
+        return '<br>'
+    
+class Chars(object):
+    def __init__(self,characters):
+        self.characters = characters
+        
+    def getCharacters(self):
+        return self.characters
+    
+    def __str__(self):
+        return('\n chars:'+str(self.characters))
+    
+    def __repr__(self):
+        return self.characters
+    
+    def htmlstr(self,token):
+        return self.characters
+    
+class Room_desc(object):
+    def __init__(self,itemlist):
+        self.itemlist = itemlist
+        
+    def getItemList(self):
+        return self.itemlist
+    
+    def __str__(self):
+        itemstr = []
+        for i in self.itemlist:
+            itemstr.append('\n room_desc item:'+str(i))
+        return ''.join(itemstr)
+    
+    def __repr__(self):
+        return ''.join([repr(i) for i in self.itemlist])
+    
+    def htmlstr(self,token):
+        return ''.join([i.htmlstr(token) for i in self.itemlist])
+    
+                                 
+class Room(object):
+    def __init__(self,name,room_desc):
+        self.name = name
+        self.room_desc = room_desc
+        self.links = []
+        for i in room_desc.itemlist:
+            if type(i) == Link:
+                self.links.append(repr(i.linkto))
+        print(self.links)
+    
+    def getName(self):
+        return self.name
+    
+    def getRoom_desc(self):
+        return self.room_desc
+    
+    def __str__(self):
+        return('\n room name:' + str(self.name)+', room description:' + str(self.room_desc))
+    
+    def __repr__(self):
+        return '#%s\n%s' %(repr(self.name),repr(self.room_desc))
+    
+    def htmlstr(self,token):
+        return '''<h1>%s</h1><br>%s''' % (self.name.htmlstr(token),self.room_desc.htmlstr(token))
+    
+
+class Item(object):
+    def __init__(self,name,description):
+        self.name = name
+        self.description = description
+        
+    def getName(self):
+        return self.name
+    
+    def __str__(self):
+        return('\n object name:' + str(self.name) + ' Object description' + str(self.description))
+    
+    def __repr__(self):
+        return '##%s\n%s' % (repr(self.name),repr(self.description))
+        
+class Text_Adventure(object):
+    def __init__(self,itemlist):
+        self.itemlist = itemlist
+        self.rooms = {}
+        self.items = {}
+        for thing in itemlist:
+            if type(thing) == Room:
+                self.rooms[repr(thing.name)] = thing
+            else:
+                self.items[repr(thing.name)] = thing
+        print(self.rooms)
+        
+    def getItemList(self):
+        return self.itemlist
+    
+    def __str__(self):
+        itemstr = []
+        num = 0
+        for i in self.itemlist:
+            num += 1
+            print(str(num)+str(i))
+#             itemstr.append(str(num)+str(i))
+        return ''
+    
+    def __repr__(self):
+        return '\n\n'.join([repr(i) for i in self.itemlist]) + '\n\n'
+        
+    def htmlstr(self,token):
+        htmldict = {}
+        for thing in self.itemlist:
+            htmldict[repr(thing.name)] = thing.htmlstr(token)
+        return htmldict
+
+class Link(object):
+    def __init__(self,linkto,alttext=None):
+        self.linkto = linkto
+        self.alttext = alttext
+        
+    def __str__(self):
+        if self.alttext == None:
+            return 'links to:'+str(self.linkto)
+        else:
+            return 'links to:'+str(self.linkto)+' alt text:'+str(self.alttext)
+        
+    def __repr__(self):
+        if self.alttext == None:
+            return '[[%s]]' %(repr(self.linkto))
+        else:
+            return '[[%s|%s]]' %(repr(self.linkto),repr(self.alttext))
+        
+    def htmlstr(self,token):
+        if self.alttext == None:
+            return '<a href="/%s/rooms/%s">%s</a>' %(token,self.linkto.htmlstr(token),self.linkto.htmlstr(token))
+        else:
+            return '<a href="/%s/rooms/%s">%s</a>' %(token,self.linkto.htmlstr(token),self.alttext.htmlstr(token))
+        
+class Guard(object):
+    def __init__(self,conditions,guard_desc):
+        self.conditions = conditions
+        self.guard_desc = guard_desc
+        
+    def  __str__(self):
+        return 'conditions:%s guard_desc: %s' % (str(self.conditions),str(self.guard_desc))
+        
+    def __repr__(self):
+        return '@%s/%s/' % (repr(self.conditions),repr(self.guard_desc))
+    
+class ItemInRoom(object):
+    def __init__(self):
+        pass
+    
+class Player(object):
+    def __init__(self,inventory = [], room = 'start'):
+        self.inventory = inventory
+        self.room = room
+        
+    def __call__(self,textadventure):
+        inputed = ''
+        while inputed != 'quit':
+            print(repr(textadventure.rooms[self.room]))
+            for link in textadventure.rooms[self.room].links:
+                print('you can go to ' + link)
+            inputed = input('>>')
+            if inputed in textadventure.rooms[self.room].links:
+                self.room = inputed
+            else:
+                print("I'm sorry, you can not go there")
+
+# Defining the parser ##################################################################################################
+def parser(lexedstring):
+    return(text_adventure(0,lexedstring))
+
+def text_adventure(i,lexedstring):
+    itemlist = []
+    while i < len(lexedstring):
+        try:
+            nexti,newroom = room(i,lexedstring)
+            itemlist.append(newroom)
+        except ParsingError:
+            nexti,newitem = item(i,lexedstring)
+            itemlist.append(newitem)
+        i = nexti
+        try:
+            if lexedstring[i][0] != 'EXIT':
+                raise ParsingError('Does not EXIT',i,lexedstring[i])
+            else:
+                i += 1
+        except IndexError:
+            raise ParsingError('Needs an extra enter/return at the end of the file')
+    return Text_Adventure(itemlist)
+    
+def room(i,lexedstring):
+    if lexedstring[i][0] == 'NEW_ROOM' and lexedstring[i+1][0] == 'CHARS' and lexedstring[i+2][0] == 'END_LINE':
+        name = lexedstring[i+1][1]
+        i += 3
+        i,itemlist = room_desc(i,lexedstring)
+        return i,Room(Chars(name),itemlist)
+    else:
+        raise ParsingError('Does not contain the proper fromat for a room')
+              
+def room_desc(i,lexedstring):
+    valid = ['CHARS','END_LINE','OPEN_OBJECT','CLOSE_OBJECT']
+    itemlist = []
+    while i < len(lexedstring):
+        if lexedstring[i][0] in valid:
+            if lexedstring[i][0] == 'CHARS':
+                itemlist.append(Chars(lexedstring[i][1]))
+            elif lexedstring[i][0] == 'END_LINE':
+                itemlist.append(Endline())
+            i += 1
+        else:
+            try:
+                i,newlink = link(i,lexedstring)
+                itemlist.append(newlink)
+            except ParsingError:
+                try:
+                    i,newguard = guard(i,lexedstring)
+                    itemlist.append(newguard)
+                    i += 1
+                except ParsingError:
+                    break
+    return i,Room_desc(itemlist)
+
+def link(i,lexedstring):
+    if lexedstring[i][0] == 'OPEN_LINK' and lexedstring[i+1][0] == 'CHARS' and lexedstring[i+2][0] == 'CLOSE_LINK':
+        return i+3,Link(Chars(lexedstring[i+1][1]))
+    elif lexedstring[i][0] == 'OPEN_LINK' and lexedstring[i+1][0] == 'CHARS' and lexedstring[i+2][0] == 'ALT_TEXT' and lexedstring[i+3][0] == 'CHARS' and lexedstring[i+4][0] == 'CLOSE_LINK':
+        return i+5,Link(Chars(lexedstring[i+1][1]),Chars(lexedstring[i+3][1]))
+    else:
+        raise ParsingError('not a link')
+    
+def item(i,lexedstring):
+    if lexedstring[i][0] == 'NEW_OBJECT' and lexedstring[i+1][0] == 'CHARS' and lexedstring[i+2][0] == 'END_LINE':
+        name = lexedstring[i+1][1]
+        i += 3
+        i,itemlist = room_desc(i,lexedstring)
+        return i,Item(Chars(name),itemlist)
+    else:
+        raise ParsingError('Does not contain the proper fromat for an object or room')
+        
+def guard(i,lexedstring):
+    if lexedstring[i][0] == 'NEW_GUARD' and lexedstring[i+1][0] == 'CHARS' and lexedstring[i+2][0] == 'GUARD_LIMIT':
+        conditions = lexedstring[i+1][1]
+        i += 3
+        i,itemlist = room_desc(i,lexedstring)
+        return i,Guard(Chars(conditions),itemlist)
+    else:
+        raise ParsingError('not a guard')
---
+++ environmock/main.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+try:
+    import webapp2
+except:
+    pass
+
+from google.appengine.ext import ndb
+
+import lexerparser
+
+example = """#start
+[[music]]
+
+#music
+The music room has a grand piano on a low stage in one corner. There is a music stand with a sheet of notes. Do you want to [[play]] the music?
+
+#play
+You hear the [[opening]] of Beethoven's Fifth Symphony 
+
+#opening
+A secret door hidden behind a large tapestry opens to a stairwell going [[down]]. 
+
+#down
+You follow the staircase down down down until you get to a [[music|music room]]
+
+"""
+
+class User(ndb.Model):
+    token = ndb.StringProperty()
+    game = ndb.TextProperty()
+
+htmldict = {}
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("""<form action="/token" method="post">
+  Username:<br>
+  <input type="text" name="username" value="johndoe45"><br>
+  Password:<br>
+  <input type="password" name="password" value="Ilovecats"><br><br>
+  <input type="submit" value="Submit">
+</form>""")
+
+class InventoryHandler(webapp2.RequestHandler):
+    def get(self,it):
+        self.response.write("""<h1>Inventory</h1>
+
+%s has:<br>
+a lamp<br>
+a disease<br>
+a meat<br>
+a water<br>
+%s
+edit the <a href="/%s/editing">game?</a>
+""" % (it,htmldict,it))
+   
+class TokenHandler(webapp2.RequestHandler):
+    def post(self):
+        password = str(self.request.get('password'))
+        username = str(self.request.get('username'))
+        token = username+password+'12'
+        query = User.query(User.token == token)
+        user = query.get()
+        if user == None:
+            user = User()
+            user.token = token
+            user.game = """#start
+Hi you must be new, have a look around and make your own game
+
+"""
+            user.put()
+        self.response.write("""Hi %s!<br>This is your <a href="/%s/game">token</a>""" % (username,token))
+
+class GameHandler(webapp2.RequestHandler):
+    def get(self,token):
+        global htmldict
+        query = User.query(User.token == token)
+        user = query.get()
+        lexedstring = lexerparser.lex(user.game)
+        textadventure = lexerparser.parser(lexedstring)
+        htmldict = textadventure.htmlstr(user.token)
+        self.response.write("""<frameset cols="50%,50%">
+  <frame src="/{token}/rooms/start" name='game'>
+  <frame src="/{token}/inventory" name='misc'>
+</frameset>""".format(token = token))
+        
+class RoomHandler(webapp2.RequestHandler):
+    def get(self,token,room):
+        self.response.write(htmldict[room])
+
+class EditingHandler(webapp2.RequestHandler):
+    def get(self,token):
+        message = str(self.request.get('message'))
+        game = str(self.request.get('game'))
+        query = User.query(User.token == token)
+        user = query.get()
+        if game == '':
+            game = user.game
+        self.response.write("""{message}<form action="/{token}/savegame" method="post">
+  Game:<br><textarea name="game">{game}</textarea><br>
+  <input type="submit" value="Submit">
+</form>""".format(token = token,message = message, game = game))
+
+class SaveHandler(webapp2.RequestHandler):
+    def post(self,token):
+        global htmldict
+        game = str(self.request.get('game'))
+        try:
+            lexedstring = lexerparser.lex(game)
+        except lexerparser.LexingError as e:
+            uri = self.uri_for('editing', token=token, message='something went wrong with lexing {e}'.format(e=str(e)), game=game )
+            self.redirect(uri)
+        else:
+            try:
+                textadventure = lexerparser.parser(lexedstring)
+            except lexerparser.ParsingError as e:
+                uri = self.uri_for('editing', token=token, message='something went wrong with parsing {e}'.format(e=str(e)), game=game)
+                self.redirect(uri)
+        htmldict = textadventure.htmlstr(token)
+        query = User.query(User.token == token)
+        user = query.get()
+        user.game = game
+        user.put()
+        self.response.write("""Your game is now <a href="/%s/rooms/start" target='game'>playable</a>""" % (token))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/([a-zA-Z0-9]+)/inventory', InventoryHandler),
+    ('/token', TokenHandler),
+    ('/([a-zA-Z0-9]+)/game', GameHandler),
+    ('/([a-zA-Z0-9]+)/rooms/([a-zA-Z0-9_]+)', RoomHandler),
+#    ('/([a-zA-Z0-9]+)/editing', EditingHandler),
+    ('/([a-zA-Z0-9]+)/savegame', SaveHandler),
+    webapp2.Route(r'/<token>/editing', handler=EditingHandler, name='editing')
+
+], debug=True)
